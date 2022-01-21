6c24639bd2d99699df470fc471d744ac2c66a901
ksm2264 <karl.muller@utexas.edu>
2022-01-21 11:12:51 -0600

    Add files via upload

--- getData.sh
+++ getData.sh
@@ -6,4 +6,3 @@ mv $SCRATCH/MonocularProject/${1}/world_undistorted.mp4 $SCRATCH/MonocularProjec
 $WORK/rclone-*/rclone copy remote:BerkeleyDataBackup/${1}/Monocular/allWalks_n.mat $SCRATCH/MonocularProject/${1}/ --progress
 mkdir $SCRATCH/MonocularProject/${1}/subclips
 mkdir $SCRATCH/MonocularProject/${1}/subclip_frames
-mkdir error_logs output_logs overrides all_jobscripts
