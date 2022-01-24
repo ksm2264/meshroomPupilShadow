d04c9bd7412e6194df79aa7c044302a40e292be9
ksm2264 <karl.muller@utexas.edu>
2022-01-24 13:38:10 -0600

    Update convert_mesh_to_cloud.py

--- convert_mesh_to_cloud.py
+++ convert_mesh_to_cloud.py
@@ -13,4 +13,5 @@ obj_list = glob.glob(base_dir+'*'+os.sep+'Texturing'+os.sep+'*'+os.sep+'textured
 ply_list = [obj[:-4]+'.ply' for obj in obj_list]
 
 for ply,obj in zip(ply_list,obj_list):
-    os.system('flatpak run --user --env=QT_QPA_PLATFORM=offscreen org.cloudcompare.CloudCompare -SILENT -AUTO_SAVE OFF -O {obj_in} -SAMPLE_MESH POINTS 10000000 -C_EXPORT_FMT PLY -SAVE_CLOUDS FILE {ply_out}'.format(obj_in=obj, ply_out=ply))
+    if not os.path.exists(ply):
+        os.system('flatpak run --user --env=QT_QPA_PLATFORM=offscreen org.cloudcompare.CloudCompare -SILENT -AUTO_SAVE OFF -O {obj_in} -SAMPLE_MESH POINTS 10000000 -C_EXPORT_FMT PLY -SAVE_CLOUDS FILE {ply_out}'.format(obj_in=obj, ply_out=ply))
