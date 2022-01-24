376571cf24ea4c3280edf0199577fa634e2db5b7
ksm2264 <karl.muller@utexas.edu>
2022-01-24 12:59:00 -0600

    Update launch_meshroom_jobs_part2.py

--- launch_meshroom_jobs_part2.py
+++ launch_meshroom_jobs_part2.py
@@ -10,6 +10,18 @@ fileNames = [fileName.split(os.sep)[-1].split('.')[0] for fileName in fileList]
 
 scratch_pref = scratch_dir+os.sep+'MonocularProject'+os.sep
 
+log_list = glob.glob('error_logs/*.err')                                                             
+                                                                                                     
+exclude_list = []                                                                                    
+                                                                                                      
+for fileName in fileNames:                                                                            
+    for log in log_list:                                                                              
+        if 'gpu_'+sys.argv[1]+'_'+fileName in log:                                                   
+                                                                                                      
+            exclude_list.append(fileName) 
+            
+fileNames = [fileName for fileName in fileNames if fileName not in exclude_list]
+            
 for fileName in fileNames:
     this_js = sys.argv[1]+'_'+fileName+'_gpu_js'
     os.system('cp pattern_jobscripts/gpu_js all_jobscripts/{JS}'.format(JS=this_js))
