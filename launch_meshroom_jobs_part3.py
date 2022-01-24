7641d0df2cd5bea6a4972a550975e984e3e78be4
ksm2264 <karl.muller@utexas.edu>
2022-01-24 13:12:29 -0600

    Update launch_meshroom_jobs_part3.py

--- launch_meshroom_jobs_part3.py
+++ launch_meshroom_jobs_part3.py
@@ -10,6 +10,18 @@ fileNames = [fileName.split(os.sep)[-1].split('.')[0] for fileName in fileList]
 
 scratch_pref = scratch_dir+os.sep+'MonocularProject'+os.sep
 
+exclude_list = []                                                                                     
+                                                                                                      
+has_mesh_list = glob.glob(scratch_pref+sys.argv[1]+os.sep+'meshroom_output'+os.sep+'*'+os.sep+'Texturing'+os.sep+'*')                                                                                       
+                                                                                                      
+has_mesh_list = [this_item.split(os.sep)[7] for this_item in has_mesh_list]                           
+                                                                                                      
+for fileName in fileNames:                                                                            
+    if fileName in has_mesh_list:                                                                     
+        exclude_list.append(fileName)                                                                 
+                                                                                                      
+                                                                                                      
+fileNames = [fileName for fileName in fileNames if fileName not in exclude_list]
 for fileName in fileNames:
     this_js = sys.argv[1]+'_'+fileName+'_p3_js'
     os.system('cp pattern_jobscripts/part3_js all_jobscripts/{JS}'.format(JS=this_js))
