eb3b943b7f235facc3a9d962564673d14f163fd7
ksm2264 <karl.muller@utexas.edu>
2022-01-31 14:21:31 -0600

    Update getData.sh

--- getData.sh
+++ getData.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir $SCRATCH/MonocularProject/${1}
-$WORK/rclone-*/rclone copy remote:BerkeleyDataBackup/${1}/Monocular/Pupil/world_undistorted.mp4 $SCRATCH/MonocularProject/${1}/ --progress
-mv $SCRATCH/MonocularProject/${1}/world_undistorted.mp4 $SCRATCH/MonocularProject/${1}/world.mp4
-$WORK/rclone-*/rclone copy remote:BerkeleyDataBackup/${1}/Monocular/allWalks_n.mat $SCRATCH/MonocularProject/${1}/ --progress
+#$WORK/rclone-*/rclone copy remote:BerkeleyDataBackup/${1}/Monocular/Pupil/world_undistorted.mp4 $SCRATCH/MonocularProject/${1}/ --progress
+#mv $SCRATCH/MonocularProject/${1}/world_undistorted.mp4 $SCRATCH/MonocularProject/${1}/world.mp4
+$WORK/rclone-*/rclone copy remote:BerkeleyDataBackup/${1}/OutputFiles/Monocular_allWalks.mat $SCRATCH/MonocularProject/${1}/ --progress
 mkdir $SCRATCH/MonocularProject/${1}/subclips
 mkdir $SCRATCH/MonocularProject/${1}/subclip_frames
 mkdir error_logs all_jobscripts output_logs overrides
