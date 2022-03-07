f40126a568e78d00588610a3a9dc0408b751898f
Karl Muller <ksm2264@login2.ls6.tacc.utexas.edu>
2022-03-07 13:44:35 -0600

    added drive-shared-with-me stuff

--- uploadSubjData.sh
+++ uploadSubjData.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 tar -czvf ${1}_mesh_data.tar.gz ${SCRATCH}/MonocularProject/${1}/meshroom_output/*/Texturing/*/* ${SCRATCH}/MonocularProject/${1}/meshroom_output/*/StructureFromMotion/*/cameras.sfm
-${WORK}/rclone*/rclone copy ${1}_mesh_data.tar.gz  remote:Monocular_Berkeley_Mesh/ --progress
+${WORK}/rclone*/rclone copy ${1}_mesh_data.tar.gz  remote:Monocular_Berkeley_Mesh/ --progress --drive-shared-with-me
 tar -czvf ${1}_mat_file.tar.gz ${SCRATCH}/MonocularProject/${1}/meshroom_output/*/pupilShadowMesh.mat 
-${WORK}/rclone*/rclone copy ${1}_mat_file.tar.gz remote:Monocular_Berkeley_Mesh/ --progress
+${WORK}/rclone*/rclone copy ${1}_mat_file.tar.gz remote:Monocular_Berkeley_Mesh/ --progress --drive-shared-with-me
